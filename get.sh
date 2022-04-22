70a7685d312437668c96080978eb896ff34f0ec7
Paul Wright <pwright@redhat.com>
2022-04-22 16:07:46 +0100

    init

---
+++ get.sh
@@ -0,0 +1,7 @@
+curl https://doc.crds.dev/github.com/artemiscloud/activemq-artemis-operator/broker.amq.io/ActiveMQArtemis/v1beta1@v1.0.1 > build/ActiveMQArtemis.html
+
+curl https://doc.crds.dev/github.com/artemiscloud/activemq-artemis-operator/broker.amq.io/ActiveMQArtemisAddress/v1beta1@v1.0.1 > build/ActiveMQArtemisAddress.html
+
+curl https://doc.crds.dev/github.com/artemiscloud/activemq-artemis-operator/broker.amq.io/ActiveMQArtemisScaledown/v1beta1@v1.0.1 > build/ActiveMQArtemisScaledown.html
+
+curl https://doc.crds.dev/github.com/artemiscloud/activemq-artemis-operator/broker.amq.io/ActiveMQArtemisSecurity/v1beta1@v1.0.1 >  build/ActiveMQArtemisSecurity.html
