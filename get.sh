338d61afb7e77b1449f983fab85ea6d2d9f8334f
Paul Wright <pwright@redhat.com>
2022-07-21 16:03:29 +0100

    update

--- get.sh
+++ get.sh
@@ -1,2 +1,4 @@
 curl https://doc.crds.dev/raw/github.com/artemiscloud/activemq-artemis-operator@v1.0.4 > build/artemis-latest.yaml
 
+curl https://doc.crds.dev/raw/github.com/artemiscloud/activemq-artemis-operator@v1.0.1 > build/artemis-101.yaml
+
